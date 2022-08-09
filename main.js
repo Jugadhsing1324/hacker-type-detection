f2b6b05b537612a811442c8de8495062d22da56f
Jugadh Singh Rally <84567746+Jugadhsing1324@users.noreply.github.com>
2022-08-09 04:17:22 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,22 +2,25 @@ img="";
 status="";
 objects=[];
 
-function preload(){
-    img=loadImage("dog_cat.jpg");
-}
 
 function setup(){
-    canvas=createCanvas(640,420);
+    canvas=createCanvas(380,380);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 
-    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML="Status: Detecting Objects..."
 }
 
 function modelLoaded(){
     console.log("Model Is Loaded");
     status=true;
-    objectDetector.detect(img,gotResult);
+    
 }
 
 function gotResult(error,results){
@@ -25,39 +28,27 @@ function gotResult(error,results){
         console.log(error);
     }
         console.log(results);
-
+        objects=results;
     
 
 }
 
 function draw(){
-    image(img,0,0,640,420);
-
-    /* MANUAL CODE FOR RECTANGLES
-    //dog
-    fill("green");
-    text("Dog",50,75);
-    noFill();
-    stroke("green");
-    rect(30,60,350,350);
-
-    //cat
-    fill("blue");
-    text("Cat",550,90);
-    noFill();
-    stroke("blue");
-    rect(290,70,320,320);
-    */
-
+    image(video,0,0,380,380);
 
     if(status!=""){
-        for(i=0; i<objects.length ; i++){
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objectDetector.detect(video,gotResult);
+        for(var i=0; i<objects.length ; i++){
             document.getElementById("status").innerHTML="Status : Objects Detected !";
-            fill("red");
+            document.getElementById("number_of_objects").innerHTML="Number Of Objects Detected Are : "+objects.length;
+            fill(r,g,b);
             percent=floor(objects[i].confidence*100);
             text(objects[i].label+" accuracy is "+percent+"%",objects[i].x,objects[i].y);
             noFill();
-            stroke("red");
+            stroke(r,g,b);
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
         }
     }
